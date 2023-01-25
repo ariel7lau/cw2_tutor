a416c374a98c31a1d1f5769e4cb3f49fe9ffcb94
ariel7lau <111340444+ariel7lau@users.noreply.github.com>
2023-01-25 16:27:06 +0000

    Update __init__.py

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,26 +1,31 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, ValidationError, Regexp
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '<182732873917398719371873>'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c22021052:Yonghoon1994!@csmysql.cs.cf.ac.uk:3306/c22021052_cw2_db'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'cw2.db')
+app.config['SECRET_KEY'] = '(34879*&378^83hehj3349837rjkhekjfn)'
+app.config['SRF_ENABLED'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 db = SQLAlchemy(app)
 
 class CommentForm(FlaskForm):
   name = StringField('Name',validators=[DataRequired()])
   comment = StringField('Comment',validators=[DataRequired()])
-  submit = SubmitField('Register')
+  submit = SubmitField('Post Comment')
   
 class Comment(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-  name = db.Column(db.String, nullable=False)
-  comment = db.Column(db.Text, nullable=False)
+   id = db.Column(db.Integer, primary_key=True)
+   date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+   name = db.Column(db.String, nullable=False)
+   comment = db.Column(db.Text, nullable=False)
 
 def __repr__(self):
     return f"Comment('{self.name}', '{self.comment}')"
@@ -37,10 +42,35 @@ def about():
 
 @app.route("/comment")
 def comment():
-  return render_template('comment.html', title='Comment')
+  comment = CommentForm()
+  return render_template('comment.html', title='Comment',comment=comment)
 
-@app.route("/comment",methods=['GET','POST'])
+# @app.route("/comment",methods=['GET','POST'])
+# def commentposted():
+#   comment = CommentForm()
+#   flash('Comment posted!')
+#   name = comment.name.data
+#   iscomment = comment.comment.data
+#   print(name)
+#   print(iscomment)
+#   return render_template('comment.html',title='Comment',comment=comment)
+
+@app.route("/comment", methods=['GET', 'POST'])
 def commentposted():
   comment = CommentForm()
-  flash('Comment posted!')
+  if comment.validate_on_submit():
+    name = comment.name.data
+    iscomment = comment.comment.data
+    new_comment = Comment(name, iscomment)
+    db.session.add(new_comment)
+    db.session.commit()
+    flash('Comment posted!')
   return render_template('comment.html',title='Comment',comment=comment)
+
+@app.route("/recentcomments")
+def recentcomments():
+  allcomment = Comment.query.order_by(Comment.date.desc()).all()
+  return render_template('recentcomments.html', allcomment=allcomment)
+
+with app.app_context():
+  db.create_all()
