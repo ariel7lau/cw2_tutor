363d2e369cc10a2989eb09490530e70fde702700
ariel7lau <111340444+ariel7lau@users.noreply.github.com>
2023-01-27 14:23:14 +0000

    Update __init__.py

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -35,6 +35,13 @@ class Password(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
    password = db.Column(db.String, nullable=False)
+   
+class Project(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   year = db.Column(db.String, nullable=False)
+   project_category = db.Column(db.String, nullable=False)
+   project_name = db.Column(db.String, nullable=False)
+   project_description = db.Column(db.String, nullable=False)
 
 def __repr__(self):
     return f"Comment('{self.name}', '{self.comment}')"
@@ -44,18 +51,18 @@ def __repr__(self):
 @app.route("/index")
 def index():
   password = PasswordForm()
-  return render_template('index.html', title='Index',password=password)
+  return render_template('index.html', title='Index',form=PasswordForm(password=password))
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/index", methods=['GET', 'POST'])
 def passwordSubmit():
-  print("testing password")
   password = PasswordForm()
   inputPassword = password.password.data
   if(inputPassword == "admin"):
     flash('Welcome!')
     return redirect(url_for("home"))
   else:
+    flash('Wrong password')
     return redirect(url_for("index"))
 
 @app.route("/home")
@@ -66,21 +73,27 @@ def home():
 def about():
   return render_template('about.html', title='About')
 
+@app.route("/about")
+def project_all():
+    all_projects = Project.query.all()
+    return render_template('about.html', all_projects=all_projects)
+
+@app.route("/about", methods=['GET', 'POST'])
+def about_project():
+    year = request.form.get('year')
+    category = request.form.get('category')
+    projects = Project.query
+    if year:
+        projects = projects.filter(Project.year == year)
+    if category:
+        projects = projects.filter(Project.project_category == category)
+    return render_template('about.html', projects=projects)
+
 @app.route("/comment")
 def comment():
   comment = CommentForm()
   recentcomments = Comment.query.order_by(Comment.date.desc()).all()
   return render_template('comment.html', title='Comment',comment=comment,recentcomments=recentcomments)
-
-# @app.route("/comment",methods=['GET','POST'])
-# def commentposted():
-#   comment = CommentForm()
-#   flash('Comment posted!')
-#   name = comment.name.data
-#   iscomment = comment.comment.data
-#   print(name)
-#   print(iscomment)
-#   return render_template('comment.html',title='Comment',comment=comment)
 
 @app.route("/comment", methods=['GET', 'POST'])
 def commentposted():
@@ -97,7 +110,6 @@ def commentposted():
   
 @app.route("/comments")
 def comments():
-  
   return render_template('recentcomments.html', comment=comment)
 
 with app.app_context():
